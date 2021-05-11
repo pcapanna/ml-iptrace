2a3984c4dbf16a8eb2238cff2c621667d7c46c7a
Patricio Capanna <pcapanna@zetti.com.ar>
2021-05-11 12:16:06 -0300

    Index.html redirect to docs removed and cors=* for rest controllers

--- src/main/java/com/ml/iptrace/infrastructure/in_adapter/web/trace_ip/TraceIpController.java
+++ src/main/java/com/ml/iptrace/infrastructure/in_adapter/web/trace_ip/TraceIpController.java
@@ -2,29 +2,21 @@ package com.ml.iptrace.infrastructure.in_adapter.web.trace_ip;
 
 import com.ml.iptrace.application.in_port.ip.trace.TraceIpResponse;
 import com.ml.iptrace.application.in_port.ip.trace.TraceIpService;
-import com.ml.iptrace.domain.*;
 import com.ml.iptrace.infrastructure.WebAdapter;
 import com.ml.iptrace.infrastructure.in_adapter.web.trace_ip.dto.TraceIpWebRequestDTO;
 import com.ml.iptrace.infrastructure.in_adapter.web.trace_ip.dto.TraceIpWebResponseDTO;
 import com.ml.iptrace.infrastructure.in_adapter.web.trace_ip.exception.UnknownIpException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.time.ZonedDateTime;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @WebAdapter
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TraceIpController {
 
     private final TraceIpService traceIpService;
--- src/main/java/com/ml/iptrace/infrastructure/in_adapter/web/trace_stat/TraceStatsController.java
+++ src/main/java/com/ml/iptrace/infrastructure/in_adapter/web/trace_stat/TraceStatsController.java
@@ -5,6 +5,7 @@ import com.ml.iptrace.infrastructure.WebAdapter;
 import com.ml.iptrace.infrastructure.in_adapter.web.trace_stat.dto.StatsDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @WebAdapter
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TraceStatsController {
 
     private final GetTraceDistanceStats getTraceDistanceStats;
